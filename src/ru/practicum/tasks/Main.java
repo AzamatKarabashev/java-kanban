d3b786b153920b9831703b443175ec581043e2de
Mugen <kazamat09@yandex.ru>
2023-08-02 05:46:36 +0300

    Пятый коммит: добавили приватное поле ArrayList в класс Epic, котрое хранит в себе idSubtask которые к нему относятся; Добавили метод реализующий пополнение указанного поля; Теперь метод addNewSubtask также добавляет в указанный ArrayList свой id; Изменили override hashCode и equals с учетом внесенного поля в класс Epic;

--- src/ru/practicum/tasks/Main.java
+++ src/ru/practicum/tasks/Main.java
@@ -7,6 +7,21 @@ import ru.practicum.tasks.tester.Tester;
 
 public class Main {
 
+    /*
+    Пятый коммит:
+    добавили приватное поле ArrayList в класс Epic, котрое хранит в себе
+    idSubtask которые к нему относятся;
+    добавили метод реализующий пополнение указанного поля;
+    теперь метод addNewSubtask также добавляет в указанный ArrayList свой id;
+    изменили override hashCode и equals с учетом внесенного поля в класс Epic;
+
+    p.s.:
+    К сожалению теперь вызов метода addNewSubtask также требует сам Epic к которому относится...
+    pp.s.:
+    Надо просто перейти на HashMap-у и спокойно искать по ключам,
+    которые будут являтся id-шниками тасок/эпиков/подзадач...
+     */
+
     public static void main(String[] args) {
         Tester tester = new Tester();
         tester.test();
